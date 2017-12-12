1eaafd89b5557b9529b084c72e2c0e6952276abe
Laskin00 <ale.laskintues19@gmail.com>
2017-12-12 22:51:27 +0200

    Its working

--- lib/rsa.rb
+++ lib/rsa.rb
@@ -1,21 +1,26 @@
 require 'openssl'
+require 'base64'
 
-class RSA
-  def new_key
-    @public_key = OpenSSL::PKey::RSA.new(2048)
-    @cipher =  OpenSSL::Cipher::Cipher.new('des3')
-    @private_key = rsa_key.to_pem(cipher,'efu311')
-    @public_key = rsa_key.public_key.to_pem
-    @key_pair = private_key + public_key
+  class RSA
+    def initialize
+      new_key
+    end
+
+    def encrypt message
+      @public = OpenSSL::PKey::RSA.new File.read 'public_key.pem'
+      @public.public_encrypt(message)
+    end
+
+    def decrypt message
+      @private = OpenSSL::PKey::RSA.new File.read 'private_key.pem'
+      @private.private_decrypt(message)
+    end
+
+    def new_key
+      key = OpenSSL::PKey::RSA.new 2048
+
+      @private = open 'private_key.pem', 'w' do |io| io.write key.to_pem end
+      @public = open 'public_key.pem', 'w' do |io| io.write key.public_key.to_pem end
+    end
+
   end
-
-
-  def encrypt message
-    @public_key.public_encrypt(message)
-  end
-
-  def decrypt message
-    @public_key.public_decrypt(message)
-  end
-
-end
--- spec/RSA_spec.rb
+++ spec/RSA_spec.rb
@@ -1,10 +1,29 @@
 require "rsa"
 
 describe RSA do
+
   let(:encrypter){RSA.new}
 
-  it"checks encrypting and decrypting" do
+  it"checks for encrypt only" do
+    expect(encrypter.encrypt('Hola')).not_to eql('Hola')
+  end
+
+  it"checks for decrypt and encrypt" do
     expect(encrypter.decrypt(encrypter.encrypt('We done'))).to eql('We done')
   end
+
+  it"checks encrypting and decrypting for long messages" do
+    expect(encrypter.decrypt(encrypter.encrypt('I couldnt do the right class and class methods because I didnt get all the logic behind the RSA encrypting'))).to eql('I couldnt do the right class and class methods because I didnt get all the logic behind the RSA encrypting')
+  end
+
+  it"checks for numbers" do
+    expect(encrypter.decrypt(encrypter.encrypt('10'))).to eql('10')
+  end
+
+  it"checks for long numbers" do
+    expect(encrypter.decrypt(encrypter.encrypt('10123'))).to eql('10123')
+  end
+
+
 
 end
